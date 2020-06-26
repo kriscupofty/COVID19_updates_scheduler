6b0dc6743041b667d7783115421c59fb4700eb47
KristyS <kristys@alumni.ubc.ca>
2020-06-25 18:37:55 -0700

    add selenium tests

---
+++ tests/test_404page.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class Test404page():
+  def setup_method(self, method):
+    self.driver = webdriver.Chrome('./chromedriver')
+  
+  def teardown_method(self, method):
+    self.driver.quit()
+  
+  def test_404page(self):
+    self.driver.get("http://localhost:8000/type_anything")
+    self.driver.set_window_size(1280, 729)
+    assert self.driver.find_element(By.CSS_SELECTOR, ".title").text == "404"
+    assert self.driver.find_element(By.LINK_TEXT, "Go to homepage").text == "Go to homepage"
+    self.driver.find_element(By.LINK_TEXT, "Go to homepage").click()
+    assert self.driver.title == "BC COVID-19 Updates"
---
+++ tests/test_index_page.py
@@ -0,0 +1,36 @@
+import pytest
+import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestIndexPage():
+  def setup_method(self, method):
+    self.driver = webdriver.Chrome('./chromedriver')
+  
+  def teardown_method(self, method):
+    self.driver.quit()
+  
+  def test_index_age(self):
+    self.driver.get("http://localhost:8000/")
+    self.driver.set_window_size(1280, 729)
+    assert self.driver.title == "BC COVID-19 Updates"
+
+    assert self.driver.find_element(By.CSS_SELECTOR, ".toolbar > span").text == "Welcome"
+    
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".fa")
+    assert len(elements) == 1
+    
+    assert self.driver.find_element(By.CSS_SELECTOR, "h2").text == "Latest BC COVID-19 Update"
+    
+    pattern = re.compile(r'Number\sof\snew\scases:\s\d+\sNote:\sData\sis\sonly\savailable\son\sa\sbusiness\sday\.\sNew\scases\son\snon-business\sdays\swill\sbe\sadded\sto\sthe\snext\sbusiness\sday\.')
+    assert pattern.match(self.driver.find_element(By.CSS_SELECTOR, "h3").text)
+    
+    pattern = re.compile(r'Date:\s2020-(\d{2})-(\d{2})')
+    assert pattern.match(self.driver.find_element(By.CSS_SELECTOR, "h4").text)
+
+    pattern = re.compile(r'Last\supdated:\s.+')
+    assert pattern.match(self.driver.find_element(By.CSS_SELECTOR, "h5").text)
+
+    self.driver.find_element(By.LINK_TEXT, "Kristy").click()
+    assert self.driver.title == "kriscupofty (Kristy Siu) · GitHub"
